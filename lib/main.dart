2bd277c08475ae76794bceb94b6f35374198ce36
David Atlan <david@davipro.fr>
2023-08-04 15:08:52 +0200

    The Hero is clickable and redirects to the second page in which it is recalled.

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,30 @@ class FirstPage extends StatelessWidget {
         child: Container(
           height: 300,
           width: 300,
-          child: Image.network("https://img.ohmymag.com/article/musique/pochette-de-l-album-des-pink-floyd-dark-of-the-moon_52798d7562d1aa0907e1c57e5ed4216c397ba79e.jpg"),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+            },
+            child: Hero(
+              tag: 'album-image',
+              child: Image.network("https://img.ohmymag.com/article/musique/pochette-de-l-album-des-pink-floyd-dark-of-the-moon_52798d7562d1aa0907e1c57e5ed4216c397ba79e.jpg"),
+            ),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.black),
+      body: Hero(
+        tag: 'album-image',
+        child: Image.network("https://img.ohmymag.com/article/musique/pochette-de-l-album-des-pink-floyd-dark-of-the-moon_52798d7562d1aa0907e1c57e5ed4216c397ba79e.jpg"),
       ),
     );
   }
